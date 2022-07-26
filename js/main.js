dda4bcb10ce2e0a95b0667ef47e758ce871dd446
dev-minkyung <minkyung.web@gmail.com>
2022-07-26 16:38:06 +0900

    Start project

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/* HEADER TOGGLE MENU */
+const menuBtn = document.querySelector('.my-menu');
+const menuList = document.querySelector('.main-menu');
+
+menuBtn.addEventListener('click', function () {
+  menuList.classList.toggle('active');
+});
+
+/* Header 스크롤 */
+const header = document.querySelector(".header");
+
+window.onscroll = function() {
+  if (window.pageYOffset > 0) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+};
+
+/* Main View Silde */
+const swiper = new Swiper('.main-view', {
+  autoplay: {
+    delay: 3500,
+  },
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: "fraction",
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+});
+
